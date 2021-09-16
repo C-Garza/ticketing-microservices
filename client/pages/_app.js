174b1e0fbafd20ed90a3e4fc1ef706691d1a115d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-16 02:52:34 -0700

    Add signin page and set up getting initial props from _app and other components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-const App = ({Component, pageProps}) => {
+const AppComponent = ({Component, pageProps, currentUser}) => {
   return(
-    <Component {...pageProps} />
+    <div>
+      <h1>Header {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
   );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const {data} = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if(appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;
