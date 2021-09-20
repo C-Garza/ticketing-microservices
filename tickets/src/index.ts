8c972f334e10d862fdccf0639b7f700e482d4e47
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-19 20:57:10 -0700

    Set up init tickets service and move mongo uri as env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
     throw new Error("JWT_KEY must be defined!");
   }
   if(!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined!");
+    throw new Error("MONGO_URI must be defined");
   }
 
   try {
