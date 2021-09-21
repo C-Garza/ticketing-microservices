d905b839fa3ffe198baa8c876f529627810f23e4
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-20 18:05:40 -0700

    Set up Ticket model and creation route handler with tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@cgatickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@cgatickets/common";
+
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -10,6 +12,9 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== "test"
 }));
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all("*", (req, res) => {
   throw new NotFoundError();
