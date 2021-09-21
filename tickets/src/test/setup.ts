b85977454a25835c383f6d6efbc5181f4024a9ae
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-21 03:53:42 -0700

    Add route for updating ticket and fix global.signin function to give random id's

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -34,7 +34,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload {id, email}
   const payload = {
-    id: "fdsafdsa",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com"
   };
 
