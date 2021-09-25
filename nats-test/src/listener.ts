81f80da0b83ecf99c921f191874e4534231b7a6a
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-24 18:53:26 -0700

    Understand the basics of nats streaming server

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,41 @@
+import nats from "node-nats-streaming";
+import {randomBytes} from "crypto";
+import {TicketCreatedListener} from "./events/ticket-created-listener";
+
+console.clear();
+
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222"
+});
+
+stan.on("connect", () => {
+  console.log("Listener connected to NATS!");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  new TicketCreatedListener(stan).listen();
+
+  // const options = stan.subscriptionOptions()
+  //   .setManualAckMode(true)
+  //   .setDeliverAllAvailable()
+  //   .setDurableName("orders-service");
+
+  // const subscription = stan.subscribe("ticket:created", "queue-group-name", options);
+
+  // subscription.on("message", (msg: Message) => {
+  //   const data = msg.getData();
+
+  //   if(typeof data === "string") {
+  //     console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+  //   }
+
+  //   msg.ack();
+  // });
+});
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
+process.on('SIGUSR2', () => stan.close());
