22676505adbf9e2194409b33167d0e18b8ee236c
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-27 05:03:00 -0700

    Add orders service to k8's and skaffold orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@cgatickets/common";
+
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { deleteOrderRouter } from "./routes/delete";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== "test"
+}));
+app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
+
+app.all("*", (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
