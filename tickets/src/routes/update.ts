2aa0c4da7b8fd9a8e8164dc762f24095e47d6a2c
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-27 04:32:16 -0700

    Add event publishing with tickets service and update tests with mocks

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express";
 import { body } from "express-validator";
 import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError } from "@cgatickets/common";
 import { Ticket } from "../models/Ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -29,6 +31,12 @@ router.put("/api/tickets/:id", requireAuth, [
     price: req.body.price
   });
   await ticket.save();
+  new TicketUpdatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  });
 
   res.send(ticket);
 });
