c4f55ed623268e88d21e746cf7b9d46ee6d12b52
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-28 04:24:37 -0700

    Set up initial route handler and tests for creating order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,52 @@
 import express, {Request, Response} from "express";
+import { body } from "express-validator";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@cgatickets/common";
+import { Ticket } from "../models/Ticket";
+import { Order } from "../models/Order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
+// 15 MINUTES
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+// CREATES NEW ORDER FOR A TICKET
+router.post("/api/orders", 
+requireAuth, [
+  body("ticketId")
+    .not()
+    .isEmpty()
+    .isMongoId()
+    .withMessage("TicketId must be provided")
+],
+validateRequest,
+async (req: Request, res: Response) => {
+  const {ticketId} = req.body;
+  // Find the ticket the user is trying to order in the database
+  const ticket = await Ticket.findById(ticketId);
+  if(!ticket) {
+    throw new NotFoundError();
+  }
+  // Make sure ticket is not already reserved
+  const isReserved = await ticket.isReserved();
+  if(isReserved) {
+    throw new BadRequestError("Ticket is already reserved");
+  }
+
+  // Calculate an expiration date for order
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+  // Build the order and save to database
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket
+  });
+  await order.save();
+
+  // Publish an event saying order was created
+  res.status(201).send(order);
 });
 
 export {router as newOrderRouter};
