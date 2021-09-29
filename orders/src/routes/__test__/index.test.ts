84fcf52193deac9be4e4298aa086ff14ca5057a5
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-09-28 21:18:45 -0700

    Add route handler and tests for getting all orders of a user

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/Order";
+import { Ticket } from "../../models/Ticket";
+
+const buildTicket = async (title = "Concert", price = 20) => {
+  const ticket = Ticket.build({
+    title,
+    price
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it("fetches orders for a particular user", async () => {
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({
+      ticketId: ticketOne.id
+    })
+    .expect(201);
+  const {body: orderOne} = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({
+      ticketId: ticketTwo.id
+    })
+    .expect(201);
+  const {body: orderTwo} = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({
+      ticketId: ticketThree.id
+    })
+    .expect(201);
+  
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
