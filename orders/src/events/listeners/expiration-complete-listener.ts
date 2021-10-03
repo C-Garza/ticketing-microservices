5240e9b9fec4256cbf76af0a8f273938481eefda
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-03 05:04:57 -0700

    Add expiration:complete listener to orders service

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,33 @@
+import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from "@cgatickets/common";
+import {Message} from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/Order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if(!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled
+    });
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+        price: order.ticket.price
+      }
+    });
+
+    msg.ack();
+  }
+}
