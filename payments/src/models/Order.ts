d4fd65aa267bb92a9b3343f2e2d03ff87f289740
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-05 17:02:26 -0700

    Set up init payments service

---
+++ payments/src/models/Order.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@cgatickets/common";
+import {updateIfCurrentPlugin} from "mongoose-update-if-current";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+};
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+};
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+};
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+  }
+});
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  const {id, ...rest} = attrs;
+
+  return new Order({
+    _id: attrs.id,
+    ...rest
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export {Order};
