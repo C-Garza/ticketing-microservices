068f84705a434f2688fe58956035b598e455284d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-07 03:32:38 -0700

    Fix issue with double currentuser calls

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({currentUser}) => {
   return (
     currentUser ? (
@@ -10,12 +8,8 @@ const LandingPage = ({currentUser}) => {
   )
 };
 
-LandingPage.getInitialProps = async (context) => {
-  console.log("LANDING PAGE!");
-  const client = buildClient(context);
-  const {data} = await client.get("/api/users/currentuser");
-
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
