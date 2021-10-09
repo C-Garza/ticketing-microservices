a60b0eadd11382a7ffa1802294e889f0a1d5e3e3
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-08 20:05:25 -0700

    Modify start up message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,7 +13,7 @@ const start = async () => {
   console.log("Connected to MongoDB");
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port: 3000");
   });
 };
 
